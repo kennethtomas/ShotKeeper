0f8b4234d3bcdc82aff5262e7d035ec91c099959
Ken <kenneth.tomas@gmail.com>
2017-07-16 19:21:17 +1000

    - Created FreeThrow tests for a ShootingSession.

--- ShotKeeper/Tests/ShotKeeper.Tests/SessionTests.cs
+++ ShotKeeper/Tests/ShotKeeper.Tests/SessionTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShotKeeper.Models;
 
 namespace ShotKeeper.Tests
 {
@@ -9,22 +10,44 @@ namespace ShotKeeper.Tests
         [TestMethod]
         public void CreateNewSession()
         {
-            // new session
-            // assert freethrows = 0
-            // assert freethrows = 0
-            // assert freethrows = 0
+            ShootingSession sessh = new ShootingSession();
+            Assert.IsNotNull(sessh);
         }
 
         [TestMethod]
         public void AddFreeThrow()
         {
-
+            ShootingSession session = new ShootingSession();
+            session.NumberOfFreeThrows++;
+            Assert.AreEqual(session.NumberOfFreeThrows, 1);
         }
 
         [TestMethod]
         public void RemoveFreeThrow()
         {
+            ShootingSession session = new ShootingSession();
+            session.NumberOfFreeThrows++;
+            Assert.AreEqual(session.NumberOfFreeThrows, 1);
+            session.NumberOfFreeThrows--;
+            Assert.AreEqual(session.NumberOfFreeThrows, 0);
+        }
 
+        [TestMethod]
+        public void AddFreeThrowCounted()
+        {
+            ShootingSession session = new ShootingSession();
+            session.NumberOfFreeThrowsCounted++;
+            Assert.AreEqual(session.NumberOfFreeThrowsCounted, 1);
+        }
+
+        [TestMethod]
+        public void RemoveFreeThrowCounted()
+        {
+            ShootingSession session = new ShootingSession();
+            session.NumberOfFreeThrowsCounted++;
+            Assert.AreEqual(session.NumberOfFreeThrowsCounted, 1);
+            session.NumberOfFreeThrowsCounted--;
+            Assert.AreEqual(session.NumberOfFreeThrowsCounted, 0);
         }
     }
 }
