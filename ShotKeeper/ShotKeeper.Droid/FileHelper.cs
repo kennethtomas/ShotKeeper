30ff5dea2cf1c41cc955aa576e8a65d643ffd91a
Ken <kenneth.tomas@gmail.com>
2017-07-16 19:31:00 +1000

    - Added IFileHelper implementation for IOs and Android.

---
+++ ShotKeeper/ShotKeeper.Droid/FileHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using Xamarin.Forms;
+using ShotKeeper.Droid;
+using ShotKeeper.Interfaces;
+
+[assembly: Dependency(typeof(FileHelper))]
+namespace ShotKeeper.Droid
+{
+    public class FileHelper : IFileHelper
+    {
+        public string GetLocalFilePath(string filename)
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            return Path.Combine(path, filename);
+        }
+    }
+}
---
+++ ShotKeeper/ShotKeeper.iOS/FileHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using Xamarin.Forms;
+using ShotKeeper.iOS;
+using ShotKeeper.Interfaces;
+
+[assembly: Dependency(typeof(FileHelper))]
+namespace ShotKeeper.iOS
+{
+    public class FileHelper : IFileHelper
+    {
+        public string GetLocalFilePath(string filename)
+        {
+            string docFolder = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            string libFolder = Path.Combine(docFolder, "..", "Library", "Databases");
+
+            if (!Directory.Exists(libFolder))
+            {
+                Directory.CreateDirectory(libFolder);
+            }
+
+            return Path.Combine(libFolder, filename);
+        }
+    }
+}
